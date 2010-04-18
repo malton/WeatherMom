64a66889e1073377858d17afab14d1f356804bc3
John Britton <john@redblack.local>
2010-04-18 13:51:57 -0400

    added sample request to weather underground api for mary in the p2pu open web class

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'rest_client'
+require 'nokogiri'
+
 get '/' do
   "hello world"
+end
+
+get '/location' do
+  content_type "text/xml", :charset=>'utf-8'
+  RestClient.log = "stdout"
+  results = RestClient.get "http://api.wunderground.com/auto/wui/geo/GeoLookupXML/index.xml?query=" + params[:postal_code]
 end
